15bc842fceacfcf45b5a8c11f10a53b9fa5b2ab1
Paul Mika Rohde <30203227+paulmiro@users.noreply.github.com>
2023-09-23 18:58:53 +0200

    edit default values for dates in Transcript type

--- src/common/types.ts
+++ src/common/types.ts
@@ -4,9 +4,9 @@ export class Transcipt {
   source_language: string = '';
   task_type: string = '';
   status: string = 'new';
-  time_uploaded: Date | undefined;
+  time_uploaded: Date = new Date(0);
   processing_duration: number = 0;
-  time_processing_finished: Date | undefined;
+  time_processing_finished: Date = new Date(0);
   target_model_size?: string;
   used_model_size: string = '';
   used_device: string = '';
